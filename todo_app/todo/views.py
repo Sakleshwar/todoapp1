fb2580409d8be3e9fbb6c4b175ccf8472e3f6204
Sakleshwar <sakleshwarmarolix@gmail.com>
2023-04-24 07:31:32 +0530

    commit todoapp

---
+++ todo_app/todo/views.py
@@ -0,0 +1,16 @@
+from django.shortcuts import render, redirect
+from .models import Task
+from .forms import TaskForm
+
+def index(request):
+    tasks = Task.objects.all()
+    form = TaskForm()
+
+    if request.method == 'POST':
+        form = TaskForm(request.POST)
+        if form.is_valid():
+            form.save()
+        return redirect('index')
+
+    context = {'tasks': tasks, 'form': form}
+    return render(request, 'index.html', context)
